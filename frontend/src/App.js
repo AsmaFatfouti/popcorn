8a27ba05cfefa892e2609860ef972a2ad175cda7
AsmaFatfouti <fatfouti.asma@yahoo.com>
2022-06-19 23:24:24 -0700

    Lesson-3-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">popcorn</a>
+      </header>
+      <main>Liste des produits</main>
+    </div>
+  );
+}
+
+export default App;
