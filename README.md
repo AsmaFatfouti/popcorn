21b08c7358a04a71e0332afdba0bc7607a34fa08
AsmaFatfouti <fatfouti.asma@yahoo.com>
2022-06-21 01:23:01 -0700

    Lesson-5-List-Products

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 2. Install tools
 3. Create React App
 4. Create Git Repository
+5. List Products
+   1. Create Product Array
+   2. Add Products Images
+   3. Render Products
+   4. Style Products
---
+++ frontend/src/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679 x 829
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      Description: 'High quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 20,
+      Description: 'High quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 220,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.6,
+      numReviews: 6,
+      Description: 'High quality pant',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fit-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 380,
+      countInStock: 30,
+      brand: 'Adidas',
+      rating: 4.8,
+      numReviews: 25,
+      Description: 'High quality product',
+    },
+  ],
+};
+
+export default data;
